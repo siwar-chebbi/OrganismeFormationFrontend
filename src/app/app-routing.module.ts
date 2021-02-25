2fd4922b07c34daa4a5c7c5ab0c973f7627ff6cf
Seynave Gauthier <gauthier.seynave@gmail.com>
2021-02-25 09:28:57 +0100

    rollback to session form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { Erreur404Component } from './pages/erreur404/erreur404.component';
-import { PageInscriptionSessionComponent } from './pages/page-inscription-session/page-inscription-session.component';
 import { PageSessionComponent } from './pages/page-session/page-session.component';
 
 const routes: Routes = [
   {path:"", component:AccueilComponent},
   {path:"accueil", component:AccueilComponent},
   {path:"session", component:PageSessionComponent},
-  {path:"inscriptionSession", component:PageInscriptionSessionComponent},
   {path:"404", component:Erreur404Component},
   {path:"**", redirectTo:"404"}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { FormSessionComponent } from './components/form-session/form-session.component';
 import { ButtonModule } from 'primeng/button';
 import { HttpClientModule } from '@angular/common/http';
-import { FormInscriptionBilanComponent } from './components/form-inscription-bilan/form-inscription-bilan.component';
 import { CardFormationComponent } from './components/card-formation/card-formation.component';
 import { ListFormationComponent } from './components/list-formation/list-formation.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -15,14 +14,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { Erreur404Component } from './pages/erreur404/erreur404.component';
 import { PageSessionComponent } from './pages/page-session/page-session.component';
-import { PageInscriptionSessionComponent } from './pages/page-inscription-session/page-inscription-session.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FormSessionComponent,
-    FormInscriptionBilanComponent,
     CardFormationComponent,
     ListFormationComponent,
     HeaderComponent,
@@ -30,7 +27,6 @@ import { PageInscriptionSessionComponent } from './pages/page-inscription-sessio
     AccueilComponent,
     Erreur404Component,
     PageSessionComponent,
-    PageInscriptionSessionComponent
   ],
   imports: [
     BrowserModule,
